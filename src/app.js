8c1b716ebe5e179079fcb9aea28f023c291570eb
Gabriel Duarte <gabrielduarte1405@gmail.com>
2023-04-03 11:33:58 -0300

    Corrigindo lint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const productControler = require('./controllers/productController');
+
 const app = express();
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
 app.get('/products', productControler.findAllItens);
 app.get('/products/:id', productControler.findItensById);
 
